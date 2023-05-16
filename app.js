7a5982e5e8295bb586276e60f3599a3fa0195579
OlgaNiem <102143760+OlgaNiem@users.noreply.github.com>
2023-05-16 08:44:47 +0300

    update

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 let count = parseInt(localStorage.getItem('count') || '0');
 
-const countElement = document.getElementById('count');
-countElement.textContent = count;
+const countNumber = document.getElementById('count');
+countNumber.textContent = count;
 
 const minusButton = document.getElementById('minus');
 minusButton.addEventListener('click', () => {
   count--;
-  countElement.textContent = count;
+  countNumber.textContent = count;
   localStorage.setItem('count', count.toString());
 });
 
 const plusButton = document.getElementById('plus');
 plusButton.addEventListener('click', () => {
   count++;
-  countElement.textContent = count;
+  countNumber.textContent = count;
   localStorage.setItem('count', count.toString());
 });
